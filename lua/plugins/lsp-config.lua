2b5fead51d386d1b0ad7c51494a83eac1908493d
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2024-07-28 20:42:56 +0200

    Change hover keymap and added new for goto declaration

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -36,8 +36,9 @@ return {
         capabilities = capabilities,
       })
 
-      vim.keymap.set("n", "H", vim.lsp.buf.hover, {})
+      vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
       vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+      vim.keymap.set("n", "gD", vim.lsp.buf.declaration, {})
       vim.keymap.set("n", "gr", vim.lsp.buf.references, {})
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
     end,
