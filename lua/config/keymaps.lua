e910fee224cea010b6a3040fb71e29108b644dce
Bartlomiej Komendarczuk <bkomendarczuk@gmail.com>
2024-07-29 20:00:30 +0200

    Copilot and metals
    Changed copilot to light lua version.
    Installed metals LSP for scala

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -12,3 +12,13 @@ vim.keymap.set("n", "<C-Up>", "<C-w>k", { noremap = true, silent = true })
 
 -- Przechodzenie w prawo
 vim.keymap.set("n", "<C-Right>", "<C-w>l", { noremap = true, silent = true })
+
+vim.keymap.set("i", "<Tab>", function()
+  if require("copilot.suggestion").is_visible() then
+    require("copilot.suggestion").accept()
+  else
+    vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
+  end
+end, {
+  silent = true,
+})
